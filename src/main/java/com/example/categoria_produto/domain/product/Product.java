1d4f21abfc5891f8a9ae6abb1f68ad00f399b37e
yam ferreira <yamferreira6@gmail.com>
2026-01-24 23:52:26 -0300

    feat: create category and product

--- src/main/java/com/example/categoria_produto/domain/product/Product.java
+++ src/main/java/com/example/categoria_produto/domain/product/Product.java
@@ -1,19 +1,22 @@
-package com.example.categoria_produto.domain.category;
+package com.example.categoria_produto.domain.product;
 
+import com.example.categoria_produto.domain.category.Category;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "categories")
+@Document(collection = "products")
 @Getter
 @Setter
 @NoArgsConstructor
-public class Category {
+public class Product {
     @Id
     private String id;
     private String title;
     private String description;
     private String ownerId;
+    private Integer price;
+    private Category category;
 }

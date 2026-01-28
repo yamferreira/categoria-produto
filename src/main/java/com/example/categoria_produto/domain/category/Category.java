9a9ab0f5fc58dff4bba694c40f7069893b7a13a0
yam ferreira <yamferreira6@gmail.com>
2026-01-28 00:06:00 -0300

    fix: cannot find symbol. I took Lombook out of class Category and use Getter and Setter without annotation

--- src/main/java/com/example/categoria_produto/domain/category/Category.java
+++ src/main/java/com/example/categoria_produto/domain/category/Category.java
@@ -7,8 +7,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "categories")
-@Getter
-@Setter
+//@Getter
+//@Setter
 @NoArgsConstructor
 public class Category {
     @Id
@@ -21,5 +21,37 @@ public class Category {
         this.title = categoryDTO.title();
         this.description = categoryDTO.description();
         this.ownerId = categoryDTO.ownerId();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
     }
 }

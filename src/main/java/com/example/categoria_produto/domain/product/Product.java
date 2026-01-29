fce2628a7cd82c3b86e2f658a1ef9343020a76cb
yam ferreira <yamferreira6@gmail.com>
2026-01-28 22:56:28 -0300

    fix: cannot find symbol. I took lombok out of class Product and use Getter and Setter without annotation

--- src/main/java/com/example/categoria_produto/domain/product/Product.java
+++ src/main/java/com/example/categoria_produto/domain/product/Product.java
@@ -8,8 +8,8 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "products")
-@Getter
-@Setter
+//@Getter
+//@Setter
 @NoArgsConstructor
 public class Product {
     @Id
@@ -27,4 +27,51 @@ public class Product {
         this.price = data.price();
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
